b484e839483c15e0d23cb24112868621004030b1
Dubabbi <thdms3160@naver.com>
2025-08-31 02:50:36 +0900

    feat: 토스트 컴포넌트 구현

---
+++ src/shared/components/toast.tsx
@@ -0,0 +1,14 @@
+import CheckIcon from '@assets/icons/toast-check.svg?react';
+
+interface ToastProps {
+  message: string;
+}
+
+export default function Toast({ message }: ToastProps) {
+  return (
+    <div className="w-full flex-row-center gap-[0.8rem] rounded-[16px] bg-primary-500 px-[2rem] py-[1.6rem] shadow-md">
+      <CheckIcon className="h-[2.4rem] w-[2.4rem]" />
+      <span className="heading3-500 text-gray-50">{message}</span>
+    </div>
+  );
+}
