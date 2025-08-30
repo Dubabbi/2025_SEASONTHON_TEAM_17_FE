b5cf8939de854bc78e8cb76414e102917302c6fe
Dubabbi <thdms3160@naver.com>
2025-08-31 01:24:53 +0900

    feat: 일기 작성 완료 바텀시트 구현

--- src/pages/my-page/my-page.tsx
+++ src/pages/my-page/my-page.tsx
@@ -1,3 +1,4 @@
+import DiaryCompleteSheet from '@components/bottom-sheet/diary-complete-sheet';
 import LeaveConfirmSheet from '@components/bottom-sheet/leave-confirm-sheet';
 import LogoutConfirmSheet from '@components/bottom-sheet/logout-confirm-sheet';
 import ProfilePhotoSheet from '@components/bottom-sheet/profile-photo-sheet';
@@ -8,15 +9,18 @@ export default function ProfilePage() {
   const photoSheet = useBottomSheet();
   const leaveSheet = useBottomSheet();
   const logout = useBottomSheet();
+  const sheet = useBottomSheet();
   return (
     <div className="flex-col-center gap-[3rem]">
       <PrimaryCTA onClick={photoSheet.open}>프로필 사진 변경</PrimaryCTA>
-
       <PrimaryCTA className="bg-error-default" onClick={leaveSheet.open}>
         탈퇴하기
       </PrimaryCTA>
       <PrimaryCTA className="bg-primary-800" onClick={logout.open}>
         로그아웃
+      </PrimaryCTA>
+      <PrimaryCTA className="bg-primary-800" onClick={sheet.open}>
+        작성 완료 보기
       </PrimaryCTA>
       <ProfilePhotoSheet
         isOpen={photoSheet.isOpen}
@@ -26,7 +30,6 @@ export default function ProfilePage() {
           photoSheet.close();
         }}
       />
-
       <LeaveConfirmSheet
         isOpen={leaveSheet.isOpen}
         onClose={leaveSheet.close}
@@ -39,6 +42,18 @@ export default function ProfilePage() {
         onClose={logout.close}
         onLogout={() => {
           logout.close();
+        }}
+      />
+      <DiaryCompleteSheet
+        isOpen={sheet.isOpen}
+        onClose={sheet.close}
+        onGoRecords={() => {
+          // TODO: 기록 페이지로 이동
+          sheet.close();
+        }}
+        onGoMain={() => {
+          // TODO: 메인 페이지로 이동
+          sheet.close();
         }}
       />
     </div>
---
+++ src/shared/components/bottom-sheet/diary-complete-sheet.tsx
@@ -0,0 +1,55 @@
+import BottomSheet from '@components/bottom-sheet/bottom-sheet';
+import Button from '@components/button/button';
+import { PrimaryStrongCTA } from '@components/button/cta-button';
+import { cn } from '@libs/cn';
+
+interface DiaryCompleteSheetProps {
+  isOpen: boolean;
+  onClose: () => void;
+  /** 기록 보러 가기 */
+  onGoRecords: () => void;
+  /** 메인 이동 */
+  onGoMain?: () => void;
+  className?: string;
+}
+
+export default function DiaryCompleteSheet({
+  isOpen,
+  onClose,
+  onGoRecords,
+  onGoMain,
+  className,
+}: DiaryCompleteSheetProps) {
+  return (
+    <BottomSheet
+      isOpen={isOpen}
+      onClose={onClose}
+      className={cn('px-[2.4rem] pb-[2.2rem]', className)}
+    >
+      <div className="flex-col gap-[1.6rem] pb-[4rem]">
+        <h2 className="heading2-700 text-gray-900">작성을 완료했습니다.</h2>
+        <p className="heading3-500 text-gray-600">
+          지금 작성한 내용은 나의 감정 일기 기록 페이지에서
+          <br />
+          확인할 수 있어요. 확인해보시겠어요?
+        </p>
+      </div>
+
+      <div className="flex-col-center gap-[0.8rem]">
+        <PrimaryStrongCTA labelClassName="heading2-700" onClick={onGoRecords} className="w-full">
+          나의 감정 일기 기록 보러 가기
+        </PrimaryStrongCTA>
+
+        <Button
+          onClick={onGoMain}
+          className={cn(
+            'w-full bg-transparent py-[0.75rem] text-center',
+            'body1-500 text-gray-400',
+          )}
+        >
+          일기 메인 페이지로 이동
+        </Button>
+      </div>
+    </BottomSheet>
+  );
+}
