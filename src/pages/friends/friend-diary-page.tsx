0fc6cb22f85c2f12ae03c54fb0da1164d1303556
Dubabbi <thdms3160@naver.com>
2025-09-05 00:56:01 +0900

    feat: 친구 글 페이지 수정

--- src/pages/friends/friend-diary-page.tsx
+++ src/pages/friends/friend-diary-page.tsx
@@ -9,13 +9,14 @@ export default function FriendDiaryPage() {
   const d = useMemo(() => (date ? dayjs(date).toDate() : new Date()), [date]);
 
   return (
-    <div className="min-h-dvh flex-col gap-[1.6rem] px-[2.4rem] pt-[1.2rem] pb-[12rem]">
+    <div className="min-h-dvh flex-col gap-[2rem] px-[2.4rem] pt-[2rem] pb-[12rem]">
       <div className="flex-col gap-[1.2rem]">
         {/* TODO: 카드 컴포넌트 수정 후 삭제하기로 변경 */}
         <DiaryCard
           title="피곤하다..."
           content="아야아"
           date={d}
+          emotions={[]}
           onClickButton={() => console.log('delete my diary')}
         />
         <div className="relative">
@@ -27,7 +28,7 @@ export default function FriendDiaryPage() {
             className={cn('bg-gray-50 outline outline-primary-500')}
             onClickButton={() => console.log('open friend diary')}
           />
-          <span className="detail absolute right-[1.6rem] bottom-[1.6rem] rounded-full border border-primary-500 bg-white px-[1.2rem] py-[0.6rem] text-primary-500">
+          <span className="detail absolute right-[1.6rem] bottom-[1.6rem] rounded-full border border-primary-500 bg-gray-50 px-[1.2rem] py-[0.6rem] text-primary-500">
             From. 마몬
           </span>
         </div>
