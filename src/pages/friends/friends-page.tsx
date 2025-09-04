250da38fef95719bd77afdbda2366b14e934b694
Dubabbi <thdms3160@naver.com>
2025-09-04 10:32:05 +0900

    feat: 친구 탭 구현

--- src/pages/friends/friends-page.tsx
+++ src/pages/friends/friends-page.tsx
@@ -1,6 +1,7 @@
 import NotiBg from '@assets/images/noti-bg.png';
 import Button from '@components/button/button';
 import FriendsHeader from '@pages/friends/components/friends-header';
+import FriendsTabs from '@pages/friends/components/friends-tab';
 import SearchBar from '@pages/friends/components/search-bar';
 
 export default function FriendsPage() {
@@ -9,7 +10,7 @@ export default function FriendsPage() {
       <FriendsHeader />
       <div className="flex-col-center gap-[1.5rem] px-[2.4rem] py-[1.5rem]">
         <SearchBar placeholder="친구를 검색해 보세요" />
-        {/* 친구목록/요청한 친구 목록/요청받은 친구 목록 탭 */}
+        <FriendsTabs />
         <div className="w-full flex-row-between">
           <span className="heading1-700">내 친구 목록</span>
           <Button className="body2-500 rounded-full bg-gray-50 px-[1.6rem] py-[0.55rem] text-primary-800 outline outline-primary-800">
