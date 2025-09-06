704ffb3bb4e9f417859415f16091f6eb3f8108b4
Dubabbi <thdms3160@naver.com>
2025-09-07 03:57:41 +0900

    refactor: 필요 없는 코드 삭제

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -2,15 +2,12 @@ import TipInfo from '@components/tipinfo';
 import { MOCK_USER } from '@mocks/user';
 import EmotionStatsSection from '@pages/main/components/emotion-stats-section';
 import HeroSection from '@pages/main/components/hero-section';
-import { useState } from 'react';
 
 export default function MainPage() {
   const today = new Date();
   const formattedDate = `${today.getFullYear()}.${String(today.getMonth() + 1).padStart(2, '0')}.${String(
     today.getDate(),
   ).padStart(2, '0')}`;
-
-  const [selectedChips, setSelectedChips] = useState<string[]>([]);
 
   return (
     <div className="min-h-dvh flex-col bg-gradient-bgd2 pb-[15rem]">
